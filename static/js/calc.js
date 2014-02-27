7970d75989f9d49f73fd59ca08026e44160dac78
Heather Ullrich <uheather83@gmail.com>
2014-02-26 16:35:30 -0800

    Completed Hello World problem!

--- static/js/calc.js
+++ static/js/calc.js
@@ -1,3 +1,4 @@
+
 $(function() {
   var notLastOp = function() {
     var lastChar = $('#output').text().slice(-1);
@@ -25,21 +26,64 @@ $(function() {
     if (outputText === '02') {
       outputText = '2';
     }
-    // FIXME
-    $('#output').text('haha');
+    $('#output').text(outputText);
   });
 
   $('#three').click(function() {
     var outputText = $('#output').text() + '3';
-    // FIXME
+    if (outputText === '03') {
+      outputText = '3';
+    }
     $('#output').text(outputText);
   });
 
   $('#four').click(function() {
-    // FIXME
+    var outputText = $('#output').text() + '4';
+    if (outputText === '04') {
+      outputText = '4';
+    }
+    $('#output').text(outputText);
   });
 
-  // WRITE CODE HERE for buttons 5-9
+  $('#five').click(function() {
+    var outputText = $('#output').text() + '5';
+    if (outputText === '05') {
+      outputText = '5';
+    }
+    $('#output').text(outputText);
+  });
+
+  $('#six').click(function() {
+    var outputText = $('#output').text() + '6';
+    if (outputText === '06') {
+      outputText = '6';
+    }
+    $('#output').text(outputText);
+  });
+
+ $('#seven').click(function() {
+    var outputText = $('#output').text() + '7';
+    if (outputText === '07') {
+      outputText = '7';
+    }
+    $('#output').text(outputText);
+  });
+
+ $('#eight').click(function() {
+    var outputText = $('#output').text() + '8';
+    if (outputText === '08') {
+      outputText = '8';
+    }
+    $('#output').text(outputText);
+  });
+
+  $('#nine').click(function() {
+    var outputText = $('#output').text() + '9';
+    if (outputText === '09') {
+      outputText = '9';
+    }
+    $('#output').text(outputText);
+  });
 
   $('#add').click(function() {
     if (notLastOp()) {
@@ -48,7 +92,33 @@ $(function() {
     }
   });
 
-  // WRITE CODE HERE for sub,mul,div,mod
+  $('#sub').click(function() {
+    if (notLastOp()) {
+      var outputText = $('#output').text() + '-';
+      $('#output').text(outputText);
+    }
+  });
+
+  $('#mul').click(function() {
+    if (notLastOp()) {
+      var outputText = $('#output').text() + '*';
+      $('#output').text(outputText);
+    }
+  });
+
+  $('#div').click(function() {
+    if (notLastOp()) {
+      var outputText = $('#output').text() + '/';
+      $('#output').text(outputText);
+    }
+  });
+
+$('#mod').click(function() {
+    if (notLastOp()) {
+      var outputText = $('#output').text() + '%';
+      $('#output').text(outputText);
+    }
+  });
 
   $('#sqrt').click(function() {
     var outputText = $('#output').text();
@@ -60,14 +130,14 @@ $(function() {
   $('#square').click(function() {
     var outputText = $('#output').text();
     var outputNum = parseFloat(eval(outputText));
-    // FIXME
-    var newNum = Math.sqrt(outputNum);
+    var newNum = Math.pow(outputNum,2);
     $('#output').text(newNum);
   });
 
   $('#equals').click(function() {
-    // FIXME
-    var res = eval('4*10+7');
+    var outputText = $('#output').text();
+    var outputNum = parseFloat(eval(outputText));
+    var res = (eval(outputNum));
     $('#output').text(res);
   });
 
---
+++ static/js/stuff.js
@@ -0,0 +1,24 @@
+alert('Here it is!'); // edit me!
+
+// Problem 1 (Say Hello!) ---------------------------------------------------
+$('#say_hello').click(function() {
+  alert('Hello World!')
+});
+
+
+// Problem 2 (Houdini) ------------------------------------------------------
+$('#disappear').click(function() {
+  //WRITE CODE HERE
+});
+
+$('#reappear').click(function() {
+  //WRITE CODE HERE
+});
+
+
+// Problem 3 (Tickle Me Pink) -----------------------------------------------
+// WRITE CODE HERE
+
+
+// Problem 4 (Greet Me) -----------------------------------------------------
+// WRITE CODE HERE
