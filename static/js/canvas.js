66e36b2dd0d930dd02bf7d4894456032ac2c31e2
Heather Ullrich <uheather83@gmail.com>
2014-03-05 19:12:29 -0800

    Drawing Shapes!

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -16,6 +16,27 @@ $('#p2').click(function() {
 	context.fillStyle = 'pink';
     context.strokeRect(100,125,40,40);
   });
+
+$('#p3').click(function() {
+    context.beginPath();
+    context.arc(30,30,20,2,6);
+    context.stroke();
+    context.closePath();
+  });
+
+$('#p4').click(function() {
+    context.beginPath();
+    context.arc(200,200,50,0,2*Math.PI);
+    context.stroke();
+    context.closePath();
+  });
+
+$('#p5').click(function() {
+    context.moveTo(80,80);
+    context.lineTo(15,15);
+    context.strokeStyle = 'orange';
+    context.stroke();
+  });
   //---------------------------------------------------------------------------
   //Write your code for p1-p12 here
   //
